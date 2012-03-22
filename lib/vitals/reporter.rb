fa5d441fd853629f14d9273c5cf24d43910cf5bb
Ryan Boucher <ryan.boucher@distributedlife.com>
2012-03-22 16:30:24 +1100

    derp

--- lib/vitals/reporter.rb
+++ lib/vitals/reporter.rb
@@ -28,7 +28,8 @@ module Vitals
 				ap args[4][:sql]
 #				ap args
 				ap caller[2]
-				ap called[2..-1]
+				ap caller[2..-1]
+				ap Rails.backtrace_cleaner.clean(caller[2]).first
 				ap Rails.backtrace_cleaner.clean(caller[2..-1]).first
 			elsif args.first == "render_partial.action_view"
 				ap File.basename(args[4][:identifier].gsub(".", "_"))
