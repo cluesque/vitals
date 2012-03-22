ddc015ba6e2bd058b10a627f234348f20208344a
Ryan Boucher <ryan.boucher@distributedlife.com>
2012-03-22 16:41:51 +1100

    derp

--- lib/vitals/reporter.rb
+++ lib/vitals/reporter.rb
@@ -26,24 +26,17 @@ module Vitals
 				return if args[4][:name] == "SCHEMA"
 				ap args[4][:name]
 				ap args[4][:sql]
-#				ap args
-				ap "*" * 80
 				Rails.backtrace_cleaner.clean(caller[2..-1]).each { |l| ap l}
-				#ap "*" * 80
-				#Rails.backtrace_cleaner.clean(caller[-1]).each { |l| ap l}
-				#ap "*" * 80
-				#Rails.backtrace_cleaner.clean(caller[2]).each { |l| ap l}
 			elsif args.first == "render_partial.action_view"
 				ap File.basename(args[4][:identifier].gsub(".", "_"))
 			else
-				#ap args.first
 				return
 			end		
 		
       delta = args[2] - args[1]
       delta = (delta > 0) ? delta : 0
 			ap "#{delta}s"
-
+			ap "*" * 80
       @stats.timing(args[0], delta)
     end
   end
