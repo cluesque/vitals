f905fe49fdf4c7fef8e7256b927e9f2759b561f5
Ryan Boucher <ryan.boucher@distributedlife.com>
2012-03-22 17:30:43 +1100

    derp

--- lib/vitals/reporter.rb
+++ lib/vitals/reporter.rb
@@ -22,10 +22,6 @@ module Vitals
     def report!(args)
 			if args.first == "process_action.action_controller"
 				name = "#{args[4][:controller]}.#{args[4][:action]}"
-				#ap args[4][:path]
-				#ap args[4][:status]
-				#ap "view_runtime: #{args[4][:view_runtime]}ms"
-				#ap "db_runtime: #{args[4][:db_runtime]}ms"
 
 				puts "#{get_metric_prefix}.#{name}.view"
 				puts "#{get_metric_prefix}.#{name}.db"
@@ -53,8 +49,7 @@ module Vitals
 						name = "#{classname}.#{method}"
 					end
 				end
-				ap name
-				#ap args[4][:sql]
+				puts "#{get_metric_prefix}.#{name}"
 			elsif args.first == "render_partial.action_view"
 				name = File.basename(args[4][:identifier].gsub(".", "_"))
 
@@ -65,7 +60,7 @@ module Vitals
 		
       delta = args[2] - args[1]
       delta = (delta > 0) ? delta : 0
-			ap "#{delta}s"
+			ap delta
 			ap "*" * 80
 
       @stats.timing(args[0], delta)
