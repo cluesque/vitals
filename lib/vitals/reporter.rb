fcc8ee7cae54c6db8db961629b93b14844b14bba
Ryan Boucher <ryan.boucher@distributedlife.com>
2012-03-22 16:21:27 +1100

    try and get sql method

--- lib/vitals/reporter.rb
+++ lib/vitals/reporter.rb
@@ -17,19 +17,20 @@ module Vitals
 
     def report!(args)
 			if args.first == "process_action.action_controller"
-				ap "CONTROLLER IS FINITO"
 				ap "#{args[4][:controller]}.#{args[4][:action]}"
+				ap args[4][:path]
 				ap args[4][:status]
-				ap "#{args[4][:view_runtime]}ms"
-				ap "#{args[4][:db_runtime]}ms"
+				ap "view_runtime: #{args[4][:view_runtime]}ms"
+				ap "db_runtime: #{args[4][:db_runtime]}ms"
 			elsif args.first == "sql.active_record"
 				return if args[4][:name] == "SCHEMA"
-				ap "DAS querize!"
 				ap args[4][:name]
 				ap args[4][:sql]
 				ap args
+				Rails.backtrace_cleaner.clean(caller[2..-1]).each do |line
+					ap line
+				end
 			elsif args.first == "render_partial.action_view"
-				ap "MIS VIEWS"
 				ap File.basename(args[4][:identifier].gsub(".", "_"))
 			else
 				ap args.first
