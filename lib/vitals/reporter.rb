a5c3e4e9188d19bddc5f14d4bbf93150772905c7
Ryan Boucher <ryan.boucher@distributedlife.com>
2012-03-22 15:30:11 +1100

    adjusted dumping

--- lib/vitals/reporter.rb
+++ lib/vitals/reporter.rb
@@ -15,10 +15,24 @@ module Vitals
       @stats = Statsd.new(host, port)
     end
     def report!(args)
-			ap args
+			if args.first == "process_action.action_controller"
+				ap "CONTROLLER IS FINITO"
+				ap args[4]
+			elsif args.first == "sql.active_record"
+				ap "DAS querize!"
+				ap args
+			elsif args.first == "partial.action_view"
+				ap "MIS VIEWS"
+				ap args[4]
+			else
+				ap "OTHRE"
+				ap args			
+			end		
+		
       delta = args[2] - args[1]
       delta = (delta > 0) ? delta : 0
 			ap delta
+
       @stats.timing(args[0], delta)
     end
   end
