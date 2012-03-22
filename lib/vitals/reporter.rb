800563711abd43f4032fcab70853ce31ba530428
Ryan Boucher <ryan.boucher@distributedlife.com>
2012-03-22 20:51:43 +1100

    refactoring and sending actual stats

--- lib/vitals/reporter.rb
+++ lib/vitals/reporter.rb
@@ -17,51 +17,75 @@ module Vitals
 
 		def get_metric_prefix
   		"#{ENV['COMPANY']}.#{ENV['PRODUCT']}.#{ENV['ENV']}.#{ENV['RELEASE']}.metrics"
-		end
+    end
+
+    def metrics_for_controllers args
+      name = "#{args[4][:controller]}.#{args[4][:action]}"
+
+      if args[4][:status] == 200
+        @stats.increment("#{get_metric_prefix}.#{name}.response.success")
+      else
+        @stats.increment("#{get_metric_prefix}.#{name}.response.other")
+      end
+
+      @stats.timing("#{get_metric_prefix}.#{name}.view", args[4][:view_runtime])
+      @stats.timing("#{get_metric_prefix}.#{name}.db", args[4][:db_runtime])
+      @stats.timing("#{get_metric_prefix}.#{name}.action", calculate_delta(args))
+    end
+
+    def metrics_for_models args
+      return if args[4][:name] == "SCHEMA"
+
+      call_stack = Rails.backtrace_cleaner.clean(caller[2..-1])
+      if call_stack.empty?
+        name = args[4][:name].gsub(" Load", ".find")
+      else
+        classname = File.basename(call_stack.first.split(":").first.gsub(".rb", "").gsub(".", "_"))
+        method = call_stack.first.scan(/`.*'/).first.gsub("`","").gsub("'","").gsub(" ","_")
+        name = "#{classname}.#{method}"
+      end
+      #if args[4][:name].nil?
+      #  #classname = File.basename(call_stack.first.split(":").first.gsub(".rb", "").gsub(".", "_"))
+      #  method = call_stack.first.scan(/`.*'/).first.gsub("`","").gsub("'","").gsub(" ","_")
+      #
+      #  name = "#{classname}.#{method}"
+      #else
+      #  if call_stack.empty?
+      #    name = args[4][:name].gsub(" Load", ".find")
+      #  else
+      #    #classname = File.basename(call_stack.first.split(":").first.gsub(".rb", "").gsub(".", "_"))
+      #    method = call_stack.first.scan(/`.*'/).first.gsub("`","").gsub("'","").gsub(" ","_")
+      #
+      #    name = "#{classname}.#{method}"
+      #  end
+      #end
+
+      @stats.timing("#{get_metric_prefix}.#{name}", calculate_delta(args))
+    end
+
+    def metrics_for_views args
+      name = File.basename(args[4][:identifier].gsub(".", "_"))
+
+      @stats.timing("#{get_metric_prefix}.#{name}", calculate_delta(args))
+    end
+
+    def calculate_delta args
+      delta = args[2] - args[1]
+
+      (delta > 0) ? delta : 0
+    end
 
     def report!(args)
-			if args.first == "process_action.action_controller"
-				name = "#{args[4][:controller]}.#{args[4][:action]}"
-
-				puts "#{get_metric_prefix}.#{name}.view"
-				puts "#{get_metric_prefix}.#{name}.db"
-				puts "#{get_metric_prefix}.#{name}.action"
-
-				if args[4][:status] == 200
-					puts "#{get_metric_prefix}.#{name}.success"
-				else
-					puts "#{get_metric_prefix}.#{name}.other"
-				end					
-			elsif args.first == "sql.active_record"
-				return if args[4][:name] == "SCHEMA"
-
-        name = ""
-				call_stack = Rails.backtrace_cleaner.clean(caller[2..-1])
-				if args[4][:name].nil?
-					classname = File.basename(call_stack.first.split(":").first.gsub(".rb", "").gsub(".", "_"))
-					method = call_stack.first.scan(/`.*'/).first.gsub("`","").gsub("'","").gsub(" ","_")
-
-					name = "#{classname}.#{method}"
-				else
-					if call_stack.empty?
-						name = args[4][:name].gsub(" Load", ".find")
-					else
-						classname = File.basename(call_stack.first.split(":").first.gsub(".rb", "").gsub(".", "_"))
-						method = call_stack.first.scan(/`.*'/).first.gsub("`","").gsub("'","").gsub(" ","_")
-
-						name = "#{classname}.#{method}"
-					end
-				end
-			elsif args.first == "render_partial.action_view"
-				name = File.basename(args[4][:identifier].gsub(".", "_"))
-			else
-				return
-			end		
-		
-      delta = args[2] - args[1]
-      delta = (delta > 0) ? delta : 0
-
-      @stats.timing("#{get_metric_prefix}.#{name}", delta)
+      case args.first
+        when "process_action.action_controller"
+          metrics_for_controllers args
+        when "sql.active_record"
+          metrics_for_models args
+        when "render_partial.action_view"
+          metrics_for_views args
+        else
+          # do nothing
+      end
     end
   end
 end
